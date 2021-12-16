1d93b92bfd438db6512a8ae84c2dea5f92249bd4
Douglas Oliveira <83471539+dougsoa@users.noreply.github.com>
2021-12-16 16:05:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,16 @@ let musica = document.querySelector('audio')
 
 document.querySelector('.botao-play').addEventListener('click', tocarMusica)
 
+document.querySelector('.botao-pause').addEventListener('click', pausarMusica)
+
 function tocarMusica() {
   musica.play()
+  document.querySelector('.botao-pause').style.display = 'block'
+  document.querySelector('.botao-play').style.display = 'none'
+}
+
+function pausarMusica() {
+  musica.pause()
+  document.querySelector('.botao-pause').style.display = 'none'
+  document.querySelector('.botao-play').style.display = 'block'
 }
